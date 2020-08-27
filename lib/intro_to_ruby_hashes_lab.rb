40f524f11a88edba69adf9719344716e437cce6d
Oscar A Castro <oacastro2@gmail.com>
2020-08-27 17:51:50 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,13 +3,14 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  hash
+  hash[key]
 end
 
 def update_counting_hash(hash, key)
     if hash[key]
+      hash[key] = hash[key] + 1
   else
-    hash[:new_key] = key
-    hash[key] += 1 
+    hash[key] = 1 
  end 
+ hash 
 end
